c4675548d6de79dc1ee6210c6181b262c5f42065
Denis <dsher2004@gmail.com>
2022-11-10 14:21:18 +0300

    move_space_object дополнена

--- solar_model.py
+++ solar_model.py
@@ -34,9 +34,11 @@ def move_space_object(body, dt):
     """
 
     ax = body.Fx/body.m
-    body.x += 42  # FIXME: не понимаю как менять...
-    body.Vx += ax*dt
-    # FIXME: not done recalculation of y coordinate!
+    body.x += body.Vx * dt
+    body.Vx += ax * dt
+    ay = body.Fy / body.m
+    body.y += body.Vy * dt
+    body.Vy += ay * dt
 
 
 def recalculate_space_objects_positions(space_objects, dt):
