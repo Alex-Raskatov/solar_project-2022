974cd7f1b3049261a766809a8ccf0e1514f11e07
Denis <dsher2004@gmail.com>
2022-11-10 13:55:50 +0300

    написаны функции parse_star_parameters и parse_planet_parameters

--- solar_input.py
+++ solar_input.py
@@ -1,6 +1,5 @@
 # coding: utf-8
 # license: GPLv3
-#
 
 from solar_objects import Star, Planet
 
@@ -24,6 +23,10 @@ def read_space_objects_data_from_file(input_filename):
                 star = Star()
                 parse_star_parameters(line, star)
                 objects.append(star)
+            elif object_type == "planet":
+                planet = Planet()
+                parse_planet_parameters(line, planet)
+                objects.append(planet)
             else:
                 print("Unknown space object")
 
@@ -31,6 +34,7 @@ def read_space_objects_data_from_file(input_filename):
 
 
 def parse_star_parameters(line, star):
+
     """Считывает данные о звезде из строки.
     Входная строка должна иметь слеюущий формат:
     Star <радиус в пикселах> <цвет> <масса> <x> <y> <Vx> <Vy>
@@ -45,7 +49,13 @@ def parse_star_parameters(line, star):
     **star** — объект звезды.
     """
 
-    pass  # FIXME: not done yet
+    star.R = line.split()[1]
+    star.color = line.split()[2].upper()
+    star.m = line.split()[3]
+    star.x = line.split()[4]
+    star.y = line.split()[5]
+    star.Vx = line.split()[6]
+    star.Vy = line.split()[7]
 
 def parse_planet_parameters(line, planet):
     """Считывает данные о планете из строки.
@@ -62,7 +72,14 @@ def parse_planet_parameters(line, planet):
     **line** — строка с описание планеты.
     **planet** — объект планеты.
     """
-    pass  # FIXME: not done yet...
+
+    planet.R = line.split()[1]
+    planet.color = line.split()[2].upper()
+    planet.m = line.split()[3]
+    planet.x = line.split()[4]
+    planet.y = line.split()[5]
+    planet.Vx = line.split()[6]
+    planet.Vy = line.split()[7]
 
 
 def write_space_objects_data_to_file(output_filename, space_objects):
